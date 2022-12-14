2ca31a140d17ae40f25f2b3b98fba727b242059b
Leslie <leslie@iMac-de-Leslie.local>
2022-12-13 18:41:14 -0600

    new endpoint user

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,10 +4,12 @@ const authRoutes = require("./auth.routes")
 const productRoutes = require("./product.routes")
 const reviewRoutes = require("./review.routes")
 const storeRoutes = require("./store.routes")
+const userRoutes = require("./user.routes")
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 router.use("/auth", authRoutes);
+router.use("/user",userRoutes)
 router.use("/products", productRoutes);
 router.use("/reviews", reviewRoutes);
 router.use("/stores", storeRoutes);
